019f582bb4a36c171f25eadcb500ec3d3d064ca5
Danny Barrett <jamesdbarrett1992@gmail.com>
2021-08-12 01:38:11 -0700

    fixing hamburger svg code

--- components/nav.js
+++ components/nav.js
@@ -7,8 +7,8 @@ export default function Nav() {
         <button className={styles.financeButton}>Finance</button>
         <button className={styles.hamburgerButton} aria-label="dropdown menu">
           <svg width="36px" height="24px" viewBox="0 0 36 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
-            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-              <g id="Home---Mobile" transform="translate(-258.000000, -22.000000)" fill="#000000" fill-rule="nonzero">
+            <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+              <g id="Home---Mobile" transform="translate(-258.000000, -22.000000)" fill="#000000" fillRule="nonzero">
                 <g id="menu_black_48dp" transform="translate(252.000000, 10.000000)">
                   <path d="M6,36 L42,36 L42,32 L6,32 L6,36 Z M6,26 L42,26 L42,22 L6,22 L6,26 Z M6,12 L6,16 L42,16 L42,12 L6,12 Z" id="Shape"></path>
                 </g>
