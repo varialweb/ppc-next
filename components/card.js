ae929161171b6d7b5b861b0f9ae6593156a82b74
Danny Barrett <jamesdbarrett1992@gmail.com>
2021-08-12 03:59:59 -0700

    adding cards

---
+++ components/card.js
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import styles from '../styles/card.module.scss'
+
+export default function Card({ title, snip, img}) {
+  const link = `/${title.toLowerCase()}/`
+  return (
+    <article className={styles.card}>
+      <div className={styles.imgWrapper}>
+        <div></div>
+        <img 
+          src={img.src}
+          alt={img.alt}
+          width="608"
+          height="455"
+          loading="lazy"
+        />
+      </div>
+      <h3>{title}</h3>
+      <p class={styles.snip}>{snip}</p>
+      <Link href={link}>
+        <button>View More</button>
+      </Link>
+    </article>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 // import Image from 'next/image'
 import Layout from '../components/layout'
+import Card from '../components/card'
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
@@ -21,10 +22,98 @@ export default function Home() {
             alt="Two giant sunrooms on a mansion"
         />
       </section>
-      <h1>Transform Your Outdoor Living</h1>
-      <p>
-
-      </p>
+      <main className={styles.main}>
+        <h1>Transform Your Outdoor Living</h1>
+        <p>
+          Here in the Pacific Northwest, we have beautiful landscapes and stunning views.
+          You should be able to enjoy your own patio area, no matter what the weather brings us.
+        </p>
+        <p>
+          We are a home improvement company that is based in Olympia, WA.
+          We serve Thurston County, Pierce County, Lewis County, Mason County, and south King County.
+        </p>
+        <p>
+          Whether you are a homeowner or a business owner, we can help you dramatically improve your outdoor living for you or your customers.
+          We are your certified Four Seasons Sunroom Dealer & Outdoor Living Specialists.
+        </p>
+        <section className={styles.cards}>
+          <h2 class='h2-margin-top'>Our Products</h2>
+          <Card 
+            title="Sunrooms"
+            snip="
+              A sunroom addition is a great way to maximize more space on your home. 
+              It is the perfect home improvement that ties into your house seamlessly 
+              in the most luxurious way."
+            img={{
+              src: 'https://d35tzt7i3ppzs4.cloudfront.net/sunroom1-608w.webp',
+              alt: ''
+            }}
+          />
+          <Card 
+            title="Liferooms"
+            snip="
+              Life Room is a fairly new system that is exclusive to Four Seasons Sunrooms. 
+              Having us as your certified installers with the Four Seasons Sunrooms family 
+              provides you access to this fantastic home improvement."
+            img={{
+              src: 'https://d35tzt7i3ppzs4.cloudfront.net/liferoom1-608w.webp',
+              alt: ''
+            }}
+          />
+          <Card 
+            title="Patio Covers"
+            snip="
+              The south sound receives a lot of moisture, which limits your patio enjoyment. 
+              We have the perfect solution for you."
+            img={{
+              src: 'https://d35tzt7i3ppzs4.cloudfront.net/cover1-608w.webp',
+              alt: ''
+            }}
+          />
+          <Card 
+            title="Decks"
+            snip="
+              A deck is one of the best home improvements for your patio area. 
+              Turn your space into an area that is customized to your color preference and 
+              matches your home."
+            img={{
+              src: 'https://d35tzt7i3ppzs4.cloudfront.net/deck1-608w.webp',
+              alt: ''
+            }}
+          />
+          <Card 
+            title="Power Screens"
+            snip="
+              One way to really enhance your outdoor living and patio space is to have remote 
+              controlled power screens on every side of your patio cover."
+            img={{
+              src: 'https://d35tzt7i3ppzs4.cloudfront.net/screen-1-608w.webp',
+              alt: ''
+            }}
+          />
+          <Card 
+            title="Handrails"
+            snip="
+              We install the highest quality hand railing systems available. 
+              You can highly customize your design to match your desired style."
+            img={{
+              src: 'https://d35tzt7i3ppzs4.cloudfront.net/handrail-1-608w.webp',
+              alt: ''
+            }}
+          />
+          <Card 
+            title="Retractable Awnings"
+            snip="
+              Sometimes you may not have the room to install a permanent awning or patio cover. 
+              A retractable awning may be an appropriate way for you to achieve the rain protection
+              and sun shade that you need, when you need it."
+            img={{
+              src: 'https://d35tzt7i3ppzs4.cloudfront.net/awning-1-607w.webp',
+              alt: ''
+            }}
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
