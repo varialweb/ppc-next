ee93e4c1feb5f1cb8d8520933fd4e671dda4c81a
Danny Barrett <jamesdbarrett1992@gmail.com>
2021-08-12 01:59:58 -0700

    adding preact

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { dev, isServer }) => {
+    Object.assign(config.resolve.alias, {
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat',
+    });
+
+    return config;
+  },
 }
