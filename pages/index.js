7b8a8929b5bad0e574773159d62df16ffed263b1
Danny Barrett <jamesdbarrett1992@gmail.com>
2021-08-14 04:57:09 -0700

    adding second hero img mobile

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,20 @@ export default function Home() {
     <Layout title="Transform Your Outdoor Living">
       <Head>
         <link preload="https://d35tzt7i3ppzs4.cloudfront.net/slide-img-1-q10-720w.webp" as="image"/>
+        <link preload="https://d35tzt7i3ppzs4.cloudfront.net" as="image"/>
       </Head>
       <section className={styles.hero}>
         <div className={styles.heroImgWrapper}>
           <img
             className={styles.heroImg}
             src="https://d35tzt7i3ppzs4.cloudfront.net/slide-img-1-q10-720w.webp"
+            width="720"
+            height="802"
+            alt="Two giant sunrooms on a mansion"
+          />
+          <img
+            className={styles.heroImg}
+            src="https://precisionpatios.s3.us-west-2.amazonaws.com/slide-img-2-720w.webp"
             width="720"
             height="802"
             alt="Two giant sunrooms on a mansion"
