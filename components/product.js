c69bdaad7fa6e32a56919d5f2e126b05a536d652
Danny Barrett <jamesdbarrett1992@gmail.com>
2021-09-01 02:56:02 -0700

    adding key to product page

--- components/product.js
+++ components/product.js
@@ -26,7 +26,7 @@ export default function ProductPage({ title, imgs, description, children }) {
               {
                 imgs.map((img) => {
                   return (
-                    <picture>
+                    <picture key={img.src}>
                       <source srcSet={`${base}${img.src}-1306w.webp`} media='(min-width: 1367px) and (-webkit-min-device-pixel-ratio: 2)'/>
                       <source srcSet={`${base}${img.src}-653w.webp`} media='(min-width: 1367px)'/>
                       <source srcSet={`${base}${img.src}-950w.webp`} media='(min-width: 769px) and (-webkit-min-device-pixel-ratio: 2)'/>
