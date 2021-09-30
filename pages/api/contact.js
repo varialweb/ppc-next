9a6876266655134c4df8974ad49324ea0519215c
Danny Barrett <jamesdbarrett1992@gmail.com>
2021-09-29 22:49:55 -0700

    adding mailgun.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,11 +1,14 @@
-const mailgun = require('mailgun-js')
+const Mailgun = require('mailgun.js')
+const formData = require('form-data')
+const mailgun = new Mailgun(formData)
 
 export default function handler(req, res) {
   const DOMAIN = process.env.MG_DOMAIN
   const API_KEY = process.env.MG_API_KEY
-  const mg = mailgun({
-    apiKey: API_KEY,
-    domain: DOMAIN,
+  const mg = mailgun.client({
+    username: 'api',
+    key: API_KEY,
+    // domain: DOMAIN,
   })
   const body = JSON.parse(req.body)
   const date = new Date().toString().split('').splice(4, 11).join('')
@@ -16,14 +19,14 @@ export default function handler(req, res) {
 
   const messageToLead = {
     from: `Precision Patio Covers <${process.env.EMAIL_ADDRESS}>`,
-    to: body.email,
+    to: [body.email],
     subject: 'Contact request submitted at Precision Patio Covers',
     text: `Thank you for your inquiry. Your contact request will be processed and you should receive a response within 24 hours. Have a great day!`
   }
 
   const messageToAdmin = {
     from: `Precision Patio Covers <${process.env.EMAIL_ADDRESS}>`,
-    to: process.env.EMAIL_SEND_TO,
+    to: [process.env.EMAIL_SEND_TO],
     subject: `www.precision-patios.com Contact Form Request by ${body.name} on ${date}`,
     html:
       `
@@ -39,23 +42,23 @@ export default function handler(req, res) {
       </div>`
   }
 
-  mg.messages().send(messageToLead, (error, body) => {
-    console.log(body)
-
-    if (error) {
+  mg.messages.create(DOMAIN, messageToLead)
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.error(error)
       errors = true
-      res.status(500).json({ error: error })
-    }
-  })
+    })
 
-  mg.messages().send(messageToAdmin, (error, body) => {
-    console.log(body)
-
-    if (error) {
+  mg.messages.create(DOMAIN, messageToAdmin)
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.error(error)
       errors = true
-      res.status(500).json({ error: error })
-    }
-  })
+    })
 
   if (!errors) {
     res.status(200).json({ message: 'messages sent'})
