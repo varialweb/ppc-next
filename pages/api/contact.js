668e302cf5da2cf74c372e1b7f11e122f37bec1b
Danny Barrett <dannybarrett1992@outlook.com>
2025-08-10 11:54:08 -0700

    Changed from mailgun to mailtrap

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,71 +1,36 @@
-const Mailgun = require('mailgun.js')
-const formData = require('form-data')
-const mailgun = new Mailgun(formData)
+import { MailtrapClient } from "mailtrap"
 
 export default async function handler(req, res) {
-  const DOMAIN = process.env.MG_DOMAIN
-  const API_KEY = process.env.MG_API_KEY
-  const mg = mailgun.client({
-    username: 'api',
-    key: API_KEY,
-    // domain: DOMAIN,
-  })
+  const TOKEN = process.env.MT_TOKEN
+  
+  const client = new MailtrapClient({ token: TOKEN })
   const body = JSON.parse(req.body)
   const date = new Date().toString().split('').splice(4, 11).join('')
 
-  let errors = false
-
-  // console.log('body', body)
-
-  console.log('from', process.env.EMAIL_ADDRESS)
-
   const messageToLead = {
-    from: `Precision Patio Covers <${process.env.EMAIL_ADDRESS}>`,
-    to: [body.email],
-    subject: 'Contact request submitted at Precision Patio Covers',
-    text: `Thank you for your inquiry. Your contact request will be processed and you should receive a response within 24 hours. Have a great day!`
+    from: { name: "Precision Patio Covers", email: "noreply@precision-patios.com" },
+    to: [{ email: body.email }],
+    subject: "Contact request submitted at Precision Patio Covers",
+    text: "Thank you for your inquiry. Your contact request will be processed and you should receive a response within 24 hours. Have a great day!",
   }
 
   const messageToAdmin = {
-    from: `Precision Patio Covers <${process.env.EMAIL_ADDRESS}>`,
+    from: { name: "Precision Patio Covers", email: "noreply@precision-patios.com" },
     to: [process.env.EMAIL_SEND_TO],
-    // to: ['danny@varial.dev'],
     subject: `www.precision-patios.com Contact Form Request by ${body.name} on ${date}`,
-    html:
+    text:
       `
-      <h1>Contact Form Submission</h1>
-      <h2>Contact Info</h2>
-      <div>
-        <p>Name: ${body.name}</p>
-        <p>Email: ${body.email}</p>
-        <p>City: ${body.city}</p>
-        <p>Phone: ${body.number}</p>
-      </div>
-      <h2>Message From User:</h2>
-      <div>
-        <p>${body.message}</p>
-      </div>`
+      Contact Form Submission\n\n
+      Contact Info\n
+        Name: ${body.name}\n
+        Email: ${body.email}\n
+        City: ${body.city}\n
+        Phone: ${body.number}\n
+      \n
+      Message From User:\n
+        ${body.message}\n`
   }
 
-  await mg.messages.create(DOMAIN, messageToLead)
-    .then(response => {
-      console.log(response)
-    })
-    .catch(error => {
-      console.error(error)
-      errors = true
-    })
-
-  await mg.messages.create(DOMAIN, messageToAdmin)
-    .then(response => {
-      console.log(response)
-    })
-    .catch(error => {
-      console.error(error)
-      errors = true
-    })
-
-  if (!errors) {
-    res.status(200).json({ message: 'messages sent'})
-  }
+  client.send(messageToLead).then(console.log).catch(console.error)
+  client.send(messageToAdmin).then(console.log).catch(console.error)
 }
